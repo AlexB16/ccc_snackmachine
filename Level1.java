dd47e3595ecc305e86b3d8a562042f671008faca
AlexB16 <161wit03@o365.htl-leoben.at>
2018-04-11 14:06:39 +0200

    nichts dazu gekommen

--- Level1.java
+++ Level1.java
@@ -3,8 +3,17 @@ package ccc_snackmachine;
 public class Level1 {
 
 	public static void main(String[] args) {
-			
+		int betrag = Integer.parseInt(args[0]);
+		int anzahlMuenzen = Integer.parseInt(args[1]);
+		int[] muenzen = new int[anzahlMuenzen];
+		
+		for(int m=0; m < anzahlMuenzen; m++) {
+			int offset = 2;
+			muenzen[m] = Integer.parseInt(args[offset+m]);
+		}
 
+		int geldEinwerfen;
+		
 	}
 
 }
