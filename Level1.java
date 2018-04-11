304ed5893686b65d51092714434539aa7c4ce561
AlexB16 <161wit03@o365.htl-leoben.at>
2018-04-11 15:09:52 +0200

    erstes dazu gekommen

--- Level1.java
+++ Level1.java
@@ -11,7 +11,16 @@ public class Level1 {
 			int offset = 2;
 			muenzen[m] = Integer.parseInt(args[offset+m]);
 		}
-
+			int geldEinwerfen = 0;
+			
+			for(int g = 0; g < anzahlMuenzen; g++) {
+				int diff1 = 0;
+				if (betrag < geldEinwerfen) {
+					diff1 = betrag - geldEinwerfen;
+					System.out.println("CHANGE: " + diff1);
+				}
+			
+			}
 		
 		
 		
